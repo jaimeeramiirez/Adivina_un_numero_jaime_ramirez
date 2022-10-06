1e6750b09727087b5e3355560c355b4595a19184
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-06 21:09:12 +0000

    CÓDIGO TERMINADO SIN RESTRICCIONES DE NÚMEROS, FUNCIONES Y NAME==MAIN

--- main.py
+++ main.py
@@ -2,36 +2,20 @@ print("\n\n\n")
 
 import random
 num = random.randint(0, 100)
-print("Este numero " + str(num) + " es el que debes adivinar")
+print("El número " + str(num) + " es el que debes adivinar")
 print("\n\n")
 
 while True:  # BUCLE 1
-    # Entramos en un bucle infinito
-    # que permite jugar varios turnos
+  
+    intento = int(input("Introduzca un número entre 0 y 99 incluídos: "))
 
-    while True:  # BUCLE 2
-        # Entramos en un bucle infinito
-        # que permite corregir un error de escritura
-
-        # Pedimos introducir un número
-        intento = input("Introduzca un número entre 0 y 99 incluídos: ")
-
-        try:
-            intento = int(intento)
-        except:
-            pass
-        else:
-            # Hacer la comparación
-            if 0 <= intento <= 99:
-                # Tenemos lo que queremos, salimos del BUCLE 2
-                break
-
-    # Se prueba si el intento es correcto o no
     if intento < num:
-        print("Demasiado pequeño")
+        print("Prueba con uno más grande")
     elif intento > num:
-        print("Demasiado grande")
+        print("Prueba con uno más pequeño")
     else:
-        print("HAS ACERTADO")
-        # Terminamos la partida, salimos del BUCLE 1
+        print("ENHORABUENA, HAS ACERTADO!")
+      
         break
+    
+
