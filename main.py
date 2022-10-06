e558793e19f75a0e6d7b5b60a3249798bf9f7eda
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-06 09:13:23 +0000

    Añadir diagrama de flujo

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 print("\n\n\n")
 import random 
 num = random.randint(0, 100)
+print(num)
 
-Intento= int(input("Intenta adivinar el número: "))
+intento= int(input("Intenta adivinar el número: "))
 
+
+if (intento>num):
+  print("El número que has introducido es mayor que el que tienes que adivinar")
+else: print("El número que has introducido es más pequeño que el que tienes que adivinar")
 
 
 
