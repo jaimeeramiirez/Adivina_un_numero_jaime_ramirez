c055d216d191f31aa2e97248c8348fda82f87d13
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-06 20:59:23 +0000

    Probando diferentes maneras

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 print("\n\n\n")
-import random 
+
+import random
 num = random.randint(0, 100)
-print(num)
+print("Este numero " + str(num) + " es el que debes adivinar")
+print("\n\n")
 
-intento= int(input("Intenta adivinar el número: "))
+while True:  # BUCLE 1
+    # Entramos en un bucle infinito
+    # que permite jugar varios turnos
 
+    while True:  # BUCLE 2
+        # Entramos en un bucle infinito
+        # que permite corregir un error de escritura
 
-if (intento>num):
-  print("El número que has introducido es mayor que el que tienes que adivinar")
-else: print("El número que has introducido es más pequeño que el que tienes que adivinar")
+        # Pedimos introducir un número
+        intento = input("Introduzca un número entre 0 y 99 incluídos: ")
 
+        try:
+            intento = int(intento)
+        except:
+            pass
+        else:
+            # Hacer la comparación
+            if 0 <= intento <= 99:
+                # Tenemos lo que queremos, salimos del BUCLE 2
+                break
 
-
+    # Se prueba si el intento es correcto o no
+    if intento < num:
+        print("Demasiado pequeño")
+    elif intento > num:
+        print("Demasiado grande")
+    else:
+        print("HAS ACERTADO")
+        # Terminamos la partida, salimos del BUCLE 1
+        break
