29faad9844cbf470f8b153486c7dc6d4538d0db6
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-08 11:30:38 +0000

    he añadido comentarios al programa

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ num = random.randint(0, 100)
 print("El número " + str(num) + " es el que debes adivinar")
 print("\n\n")
 
-while True:  # BUCLE 1
+while True:
   
     intento = int(input("Introduzca un número entre 0 y 99 incluídos: "))
 
     if intento < num:
         print("Prueba con uno más grande")
+        print ("\n")
+
     elif intento > num:
         print("Prueba con uno más pequeño")
+        print ("\n")
     else:
         print("ENHORABUENA, HAS ACERTADO!")
+        print ("\n")
+
       
         break
     
-
+#El código funciona, pero no hemos implementado la restricción de números entre 0 y 99, por lo que puedes introducir número mayores a 99 sin que el programa de error. Para que esto no ocurra, tendríamos que introducir otro bucle y que si el usuario introduce un número mayor o menor volver a pedirle que introduzca un número entre 0 y 99.
