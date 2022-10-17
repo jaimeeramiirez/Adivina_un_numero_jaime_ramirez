3d156fec1b4ee927ab043158839bb0c8a4514d7c
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-17 19:18:11 +0000

    tabla de puntuaciones

--- nivel_simple.py
+++ nivel_simple.py
@@ -2,11 +2,38 @@ print("\n\n\n")
 
 import random
 num = random.randint(0, 100)
-nombre_usuario= str(input("Antes de comenzar, introduzca su nombre de usuario:"))
+nombre_usuario= str(input("Antes de comenzar, introduzca su nombre de usuario: "))
 print("\n\n")
 print("El número " + str(num) + " es el que debes adivinar")
 print("\n\n")
 numero_intentos=1
+def puntuaciones():
+  tabla={}
+  tabla["Nombre del jugador"]=input("Introduce un nombre: ")
+  tabla["Puntuación: "]=puntuación
+  puntos=0
+  if numero_intentos==1:
+    puntuación = puntos +100
+  elif numero_intentos==2:
+    puntuación= puntos +90
+  elif numero_intentos==3:
+    puntuación= puntos +80
+  elif numero_intentos==4:
+    puntuación= puntos +70
+  elif numero_intentos==5:
+    puntuación= puntos +60
+  elif numero_intentos==6:
+    puntuación= puntos +50
+  elif numero_intentos==7:
+    puntuación= puntos +40
+  elif numero_intentos==8:
+    puntuación= puntos +30
+  elif numero_intentos==9:
+    puntuación= puntos +20
+  elif numero_intentos==10:
+    puntuación= puntos +10
+  else:
+    puntuación= 0
 
 def juego():
   numero_intentos=1
@@ -47,19 +74,20 @@ while True:  # BUCLE 1
     # Se prueba si el intento es correcto o no
     if numero_intentos==7:
       print("\nHAS UTILIZADO DEMASIADOS INTENTOS, \nGAME OVER")
+      puntuación=0
       break
     elif intento < num:
       numero_intentos=numero_intentos+1
       print("\nDemasiado pequeño")
+      puntuación=puntuación-10
       
     elif intento > num:
       numero_intentos= numero_intentos+1
       print("\nDemasiado grande")
+      puntuación=puntuación-10
     else:
         print("\nVictoria!")
-        print("\nHas necesitado", numero_intentos, "intentos")
+        print("\nHas necesitado", numero_intentos, "intentos\n")
         break
-print(nombre_usuario,"su puntuación es de", numero_intentos)
-
+      print 
 juego()
-nombre()
